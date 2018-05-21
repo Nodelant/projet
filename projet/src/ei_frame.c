5ad48e713fde04ae361c140fd8efa153691a15aa
Lucas.et <lucas.etourneau1@outlook.fr>
2018-05-21 15:12:56 +0200

    ok

--- projet/src/ei_frame.c
+++ projet/src/ei_frame.c
@@ -83,7 +83,7 @@ void ei_frame_setdefaultsfunc_t(struct ei_widget_t*   widget)
 //Création et enregistrement de la classe frame
 ei_widgetclass_t frame;
 // frame.ei_widgetclass_name_t = "frame";
-frame.allocfunc = &ei_frame_allocfunc_t;
+// frame.allocfunc = &ei_frame_allocfunc_t;
 // frame.releasefunc = &ei_frame_releasefunc_t;
 // frame.drawfunc = &ei_frame_drawfunc_t;
 // frame.setdefaultsfunc = &ei_frame_setdefaultsfunc_t;
